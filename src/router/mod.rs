9202840ac87e4323c0bb8cbd14a34928f48443e0
Brookzerker <brooks@brookzerker.com>
2023-11-08 16:29:50 -0700

    broken examples :)

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,11 +1,21 @@
 mod healthcheck;
 
-use axum::{routing::get, Extension, Router};
+use axum::{middleware, routing::get, Extension, Router};
 use healthcheck::healthcheck;
 
 pub fn create_router(address: [u8; 4], port: u16) -> Router {
     Router::new()
-        .route("/healthcheck", get(healthcheck))
+        .route("/healthcheck", get(healthcheck).layer(Extension(port)))
+        .merge(
+            Router::new()
+                .route("/tasks", get(get_all_tasks))
+                .route("/tasks", post(create_task)),
+        )
+        .route_layer(middleware::from_fn(firewall))
         .route_layer(Extension(address))
+        .route("/tasks/:id", get(get_task_by_id))
+        .route_layer(middleware::from_fn(authorize))
+        .route("/tasks/:id", put(update_task_by_id))
+        .route("/tasks/:id", delete(delete_task_by_id))
         .layer(Extension(port))
 }
